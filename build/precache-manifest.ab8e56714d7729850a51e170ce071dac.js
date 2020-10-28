939e907b2e5ec631b58909a5b242e372831c0933
Unknown <nguyenhoantuyetnhi@gmail.com>
2020-10-28 17:22:06 +0700

    init

---
+++ build/precache-manifest.ab8e56714d7729850a51e170ce071dac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "284526c38c7333b9cbb37be773520091",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff21488e6a572570d0bb",
+    "url": "/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "ff21488e6a572570d0bb",
+    "url": "/static/js/2.d0508e5b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d0508e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3109533d9bd46eeb6444",
+    "url": "/static/js/main.a70b7f81.chunk.js"
+  },
+  {
+    "revision": "7840a4360155d662a69e",
+    "url": "/static/js/runtime-main.49e9a94d.js"
+  }
+]);
